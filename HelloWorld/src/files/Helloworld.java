2e17c0c9a704690ed247925361a26a9519e87604
Savandi <savandi98@gmail.com>
2020-02-29 09:55:43 +0530

    My first Java program

---
+++ HelloWorld/src/files/Helloworld.java
@@ -0,0 +1,10 @@
+package files;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first project to connect to Git from eclipse");
+
+	}
+
+}
